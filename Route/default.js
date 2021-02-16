27dac13c35d9d13cd15c71a8e3b7b232b070a85e
Chintuka-Code <mohmadsabban123@gmail.com>
2021-02-16 13:31:30 +0530

    'BasisConfig'

--- Route/default.js
+++ Route/default.js
@@ -1,1 +1,10 @@
-D:\work\LearnExpressJs\Route
+const express = require('express');
+const router = express.Router();
+
+router.get('', async (req, res) => {
+  console.log('Hit');
+  const db = process.env.DB_URL;
+  res.send(db);
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-D:\work\LearnExpressJs
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+
+const app = express();
+const DB_URl = process.env.DB_URL;
+const PORT = process.env.PORT || 4000;
+
+// Inport Route
+
+const defaultRoute = require('./Route/default');
+
+app.use(cors());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+// Config base URL
+app.use('/', defaultRoute);
+
+app.listen(PORT, () => console.log(`Application Start On Port ${PORT}`));
